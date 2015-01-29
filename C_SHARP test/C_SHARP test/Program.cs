97d29b04b39becf8ba34e825188452b971c12667
Nave Tal <navetal@gmail.com>
2015-01-29 20:18:21 +0200

    Added c# stuff
    Holly shit...

---
+++ C_SHARP test/C_SHARP test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C_SHARP_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr=new int[7];
+            for(int i=1; i<=7; i++)
+            {
+                arr[i-1]=i;
+            }
+            foreach (int i in arr)
+            {
+                int b = i * i * i;
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
