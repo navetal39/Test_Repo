06c81cf0a066b6bf799bac779749f85fb09a9de5
Nave Tal <navetal@gmail.com>
2015-01-29 21:33:47 +0200

    added fib stuff
    :)

--- python scripts/hi.py
+++ python scripts/hi.py
@@ -1,1 +1,15 @@
+from math import sqrt
 print "another test. hi!"
+
+def fib(n):
+    if n<=1:
+        return n
+    return fib(n-1)+fib(n-2)
+
+def tamir(n):
+    for i in xrange(1,n+1):
+        print fib(i)
+
+for i in xrange(5):
+    tamir(i+1)
+print "lol!"
